e4d0d2e191a91cc0be8ad0d6145d97ad81331ee7
denyahnov <den.yakhnov@gmail.com>
2023-09-25 15:36:56 +1000

    Add Photo Upload

--- pit_scouter.py
+++ pit_scouter.py
@@ -7,6 +7,9 @@ import shortcuts
 
 from tba import *
 
+if not os.path.exists("imgs"):
+	os.mkdir("imgs")
+
 all_teams = TBA.get_teams()
 
 drivebases = ["Swerve", "Mecanum", "Tank", "Other"]
@@ -17,12 +20,50 @@ def pitscouter_content():
 	class Elements:
 		game_pieces = {}
 
+	def upload_photo(event):
+		Elements.spinner2.set_visibility(True)
+
+		counter = 0
+
+		while True:
+			title = "{}_{}.{}".format(Elements.team_number.value, counter, event.name.split(".")[-1])
+	
+			if not os.path.isfile(os.path.join("imgs",title)):
+				break
+
+			counter += 1
+
+		with event.content as content:
+			with open(os.path.join("imgs",title),"wb") as file:
+				file.write(content.read())
+
+		ui.notify("Uploaded {}!".format(title))
+
+		Elements.spinner2.set_visibility(False)
+
 	def save_data():
 		title = "PIT_{}".format(Elements.team_number.value)
 
 		data = {
 			"Team": Elements.team_number.value,
 			"Drivebase": Elements.drivebase.value,
+			"Pickup": {
+				"Cone": {
+					element.text: int(element.value) for element in Elements.cone_pickup
+				},
+				"Cube": {
+					element.text: int(element.value) for element in Elements.cube_pickup
+				},
+			},
+			"Score": {
+				location: int(Elements.game_pieces[location].value) for location in Elements.game_pieces
+			},
+			"Autonomous": {
+				"Has Autonomous": int(Elements.has_autonomous.value),
+				"Score Gamepiece": int(Elements.auto_score.value),
+				"Balance Charge Station": int(Elements.auto_charge_station.value),
+			},
+			"Comments": Elements.comments.value,
 		}
 
 		with open(os.path.join("data","{}.json".format(title)), "w") as file:
@@ -84,6 +125,34 @@ def pitscouter_content():
 					title = f"{location} {piece}"
 
 					Elements.game_pieces[title] = ui.switch(title)
+
+	### AUTONOMOUS ###
+
+	with ui.card():
+		ui.label("Autonomous:").classes('font-bold')
+
+		Elements.has_autonomous = ui.switch("Has Autonoumous")
+		Elements.auto_score = ui.switch("Score Gamepiece")
+		Elements.auto_charge_station = ui.switch("Balance Charge Station")
+
+	### COMMENTS ###
+
+	with ui.card():
+		Elements.comments = ui.textarea(label='Notes', placeholder='Type here...',)
+
+	### PHOTOS ###
+
+	with ui.row():
+		Elements.photo_upload = ui.upload(
+			label="Upload Robot Photos", 
+			on_upload=upload_photo, 
+			multiple=True, 
+			auto_upload=True
+		).props("accept=image/*")
+
+		Elements.spinner2 = ui.spinner(size='lg')
+
+		Elements.spinner2.set_visibility(False)
 
 	### SUBMIT DATA ###
 
