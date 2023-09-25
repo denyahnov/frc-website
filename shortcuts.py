60cc6c757485365c85f7ebff17de68d2057a0405
Camo06 <cameron.g.ellis06@gmail.com>
2023-09-25 15:26:23 +1000

    picklist style

--- shortcuts.py
+++ shortcuts.py
@@ -1,11 +1,14 @@
 from nicegui import ui
-import img
+
 def return_home():
 	ui.button("Return Home", color='orange', icon="keyboard_return", on_click=lambda: ui.open("/"))
 
 def content():
-	ui.button("Scouting App", color='orange', icon="devices", on_click=lambda: ui.open("/scouter"))
-	ui.button("Pit Scouting", color='orange', icon="devices", on_click=lambda: ui.open("/pit_scout"))
-	ui.button("Leaderboard", color='orange', icon="trending_up", on_click=lambda: ui.open("/leaderboard"))
-	ui.button("Livestream", color='orange', icon="live_tv", on_click=lambda: ui.open("/livestream"))
-	ui.button("Scouting Data", color='orange', icon="assessment", on_click=lambda: ui.open("/data"))
+	tailwind = 'w-full max-w-sm'
+
+	ui.button("Scouting App", color='orange', icon="devices", on_click=lambda: ui.open("/scouter")).classes(tailwind)
+	ui.button("Leaderboard", color='orange', icon="trending_up", on_click=lambda: ui.open("/leaderboard")).classes(tailwind)
+	ui.button("Livestream", color='orange', icon="live_tv", on_click=lambda: ui.open("/livestream")).classes(tailwind)
+	ui.button("Team Statistics", color='orange', icon="assessment", on_click=lambda: ui.open("/stats")).classes(tailwind)
+	ui.button("Picklist", icon="list", color='orange', on_click=lambda: ui.open("/picklist")).classes(tailwind)
+	ui.button("Raw Scouting Data", color='orange', icon="folder", on_click=lambda: ui.open("/data")).classes(tailwind)
